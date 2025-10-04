dde8643438488905d0161a1527d41a51dddc4442
shreyar1905 <shreya.22cs146@sode-edu.in>
2025-10-04 11:51:36 +0530

    Add files via upload

---
+++ cie3.c
@@ -0,0 +1,63 @@
+//Case1
+
+// #include <stdio.h>
+// #include <string.h>
+// #include <mpi.h>
+
+// int main(int argc, char* argv[]) {
+//     int rank;
+//     char msg[50];
+
+//     MPI_Init(&argc, &argv);
+//     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+//     if (rank == 0) {
+//         // Process 0 waits to receive first
+//         MPI_Recv(msg, 50, MPI_CHAR, 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+//         printf("Process 0 received message: %s\n", msg);
+//     }
+//     else if (rank == 1) {
+//         // Process 1 also waits to receive first
+//         MPI_Recv(msg, 50, MPI_CHAR, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+//         printf("Process 1 received message: %s\n", msg);
+//     }
+
+//     MPI_Finalize();
+//     return 0;
+// }
+
+//Case2
+#include <stdio.h>
+#include <string.h>
+#include <mpi.h>
+
+int main(int argc, char* argv[]) {
+    int rank;
+    char msg[50];
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    if (rank == 0) {
+        // Process 0 sends first
+        strcpy(msg, "Hello from Process 0");
+        MPI_Send(msg, strlen(msg)+1, MPI_CHAR, 1, 0, MPI_COMM_WORLD);
+
+        // Then receives from process 1
+        MPI_Recv(msg, 50, MPI_CHAR, 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        printf("Process 0 received message: %s\n", msg);
+    }
+    else if (rank == 1) {
+        // Process 1 receives first
+        MPI_Recv(msg, 50, MPI_CHAR, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        printf("Process 1 received message: %s\n", msg);
+
+        // Then sends back
+        strcpy(msg, "Hello from Process 1");
+        MPI_Send(msg, strlen(msg)+1, MPI_CHAR, 0, 0, MPI_COMM_WORLD);
+    }
+
+    MPI_Finalize();
+    return 0;
+}
+
